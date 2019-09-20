ae1a489bb0ded0180151d47f80417201483b81c4
wandouhua <1002649611@qq.com>
2019-09-20 12:02:43 +0800

    创建了游客登录模块

--- src/router.js
+++ src/router.js
@@ -3,13 +3,14 @@ import Router from 'vue-router'
 import Index from './views/Index.vue'
 import NotFound from './views/NotFound.vue'
 import Login from './views/Login.vue'
-import Registe from './views/Registe .vue'
+import Tourist from './views/Tourist.vue'
+import Registe from './views/Registe.vue'
 
 Vue.use(Router)
-
 export default new Router({
     routes: [
         { path: '/Registe', component: Registe },
+        { path: '/Tourist', component: Tourist },
         { path: '/Login', component: Login },
         { path: '/Index', component: Index },
         { path: '/', component: Index },

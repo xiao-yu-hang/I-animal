838eca06915cb823a1787e1931a1366ff48f84de
童海斌 <1461074392@qq.com>
2019-09-23 11:00:18 +0800

    购物车静态页面

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,47 +1,71 @@
 <template>
   <div class="container">
     <div class="cart">
-      <h1>购物车</h1>
-      <hr>
-      <p class="counts">
-        <span>
-          已选
-          <span class="count">
-            {{count}}
+      <div>
+        <h1>购物车</h1>
+        <hr>
+      </div>
+      <div>
+        <p class="counts">
+          <span>
+            已选
+            <span class="count">
+              {{count}}
+            </span>
+            件商品
           </span>
-          件商品
-        </span>
-        <span>
-          合计(不含运费)：
-          <span class="count">
-            {{price}}
+          <span>
+            合计(不含运费)：
+            <span class="count">
+              {{price}}
+            </span>
+            元
           </span>
-          元
-        </span>
-      </p>
-      <hr>
+        </p>
+        <hr>
+      </div>
       <div class="lists">
-        <div class="list">
+        <div v-for="(good,i) of goods" :key="i" class="list">
           <div class="info">
-            <img class="thumbnail" src="@/assets/logo.png">
-            <span>{{desc}}</span>
+            <img class="thumbnail" :src="good.img">
+            <span>{{good.desc}}</span>
           </div>
           <div class="option">
-            <i class="el-icon-circle-plus-outline"></i>
-            <i class="el-icon-remove-outline"></i>
+            <el-input-number v-model="good.count" @change="`goodCount`" :min="1" :max="10" label="商品计数"></el-input-number>
           </div>
         </div>
+      </div>
+      <div class="cart-option">
+        <el-button class="del" type="danger" icon="el-icon-delete">清空</el-button>
+        <el-button class="buy" type="success" icon="el-icon-goods">购买</el-button>
       </div>
     </div>
   </div>
 </template>
 <script>
+import { log } from 'util'
 export default {
   data() {
     return {
       count:0,
       price:0,
-      desc:"描述",
+      goods:[
+        {
+        img:"@/assets/logo.png",
+        desc:"描述1",
+        count:1
+        },
+        {
+        img:"@/assets/logo.png",
+        desc:"描述2",
+        count:2
+        },
+      ],
+    }
+  },
+  methods: {
+    goodCount(value){
+      console.log(value);
     }
   },
 };
@@ -55,6 +79,9 @@ export default {
   justify-content: center;
 }
 .cart{
+  display: flex;
+  flex-flow: column;
+  align-items: stretch;
   width: 600px;height: 600px;
   border: 1px solid #000;
 }
@@ -75,7 +102,27 @@ export default {
   justify-content: space-between;
   align-items: center;
 }
+.lists{
+  padding: 10px;
+}
+.list{
+  width: 100%;
+  margin: 10px;
+}
 .thumbnail{
   width: 50px;height: 50px;
+}
+.cart-option{
+  flex-grow: 1;
+  display: flex;
+  flex-flow: column;
+  justify-content: flex-end;
+}
+.del{
+  width: 100%;
+}
+.buy{
+  width: 100%;
+  margin: 0!important;
 }
 </style>
